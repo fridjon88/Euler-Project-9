9e19f46ac5481dd33a3fcb5b9a1ec26e7ac19681
fridjon88 <fridjon88@gmail.com>
2018-08-20 18:21:34 +0000

    Add project files.

---
+++ EulerProblem9WTest/ProblemServices/Problem9Service.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EulerProblem9WTest.ProblemService
+{
+    public class Problem9Service
+    {
+
+        public int FindAnswereForEuler9(int expectedSumOfTriplets)
+        {
+            for (int a = 1; a <= expectedSumOfTriplets / 3; a++)
+            {
+                for (int b = a+1; b <= expectedSumOfTriplets / 2; b++)
+                {
+                    double cDouble = FindC(a, b);
+                    if (cDouble % 1.0 != 0.0)
+                    {
+                        continue;
+                    }
+                    int c = Convert.ToInt32(cDouble);
+
+                    int tripletSum = TripletSum(a, b, c);
+                    if (tripletSum == expectedSumOfTriplets)
+                    {
+                        return ProductOfTriplet(a, b, c);
+                    }
+                    else if (tripletSum > expectedSumOfTriplets)
+                    {
+                        break;
+                    }
+                }
+            }
+            throw new NotImplementedException();
+        }
+
+         private double FindC(int a, int b)
+        {
+            return Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+        }
+
+        private int TripletSum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        private int ProductOfTriplet(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+    }
+}
---
+++ EulerProblem9WTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EulerProblem9WTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProblemService.Problem9Service problem9 = new ProblemService.Problem9Service();
+
+            Console.WriteLine(problem9.FindAnswereForEuler9(1000));
+            Console.ReadKey();
+        }
+    }
+}
